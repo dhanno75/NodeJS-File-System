153b134630b9e987c5f8fc111393be32e094d280
dhanno75 <pdhananjay774@gmail.com>
2023-01-19 18:51:47 +0530

    commit

--- index.js
+++ index.js
@@ -2,21 +2,36 @@
 import fs from "fs";
 import express from "express";
 const app = express();
+import * as dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 4000;
+const PORT = process.env.PORT;
 
 const currentTimeStamp = Date.now() + "";
 
-app.get("/", function (request, response) {
+app.get("/", (request, response) => {
   response.send("Welcome to NodeJS file system");
 });
 
-app.get("/write", function (request, response) {
+app.get("/write", (request, response) => {
   fs.writeFile("./TextFolder/date-time.txt", currentTimeStamp, (err) => {
     if (err) {
       console.error(`❌ ${err}`);
     }
     response.send("Writing successful 🎊✨");
+  });
+});
+
+app.get("/readFromDirectory", (req, res) => {
+  fs.readdir("./TextFolder", (err, files) => {
+    let fileNames = [];
+    if (err) console.error(`❌ ${err}`);
+    else {
+      console.log("\nCurrent directory filenames:");
+      files.forEach((file) => fileNames.push(file));
+      console.log(fileNames.join(", "));
+    }
+    res.send(`The filenames are ${fileNames.join(", ")}.`);
   });
 });
 
